2e3b3a605c540a45620e901a443d1e2fcc6b7c17
rajivml <46923567+rajivml@users.noreply.github.com>
2020-12-12 15:48:58 +0530

    Update rke2_centos8_dependencies.sh

--- scripts/rke2_centos8_dependencies.sh
+++ scripts/rke2_centos8_dependencies.sh
@@ -83,6 +83,16 @@ chmod +x rke2-uninstall.sh
 
 curl -sL https://raw.githubusercontent.com/rancher/rke2/488bab0f48b848e408ce399c32e7f5f73ce96129/bundle/bin/rke2-killall.sh --output rke2-killall.sh
 chmod +x rke2-killall.sh
+
+#download k9s 
+curl -sL https://github.com/derailed/k9s/releases/download/v0.24.2/k9s_Linux_x86_64.tar.gz
+sleep 5
+tar xzvf k9s_Linux_x86_64.tar.gz
+rm k9s.tar.gz
+rm k9s_Linux_x86_64.tar.gz
+rm LICENSE
+rm README.md
+
 cd ..
 #----------------- scripts end ####################
 
