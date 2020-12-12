e58366fbd93bfb017777ec827ac22ce342a5cfa7
rajivml <46923567+rajivml@users.noreply.github.com>
2020-12-12 14:11:39 +0530

    Create pull_images.sh

---
+++ scripts/pull_images.sh
@@ -0,0 +1,8 @@
+images=(`helm template . | yq -r '..|.image? | select(.)' | sort | uniq`)
+
+for image in ${images[@]};
+do
+  save_image=$(echo $image | rev | cut -d'/' -f1 | rev | tr ':' '_')
+  docker pull ${image}
+  docker save ${image} | gzip > ${save_image}.tar.gz
+done
