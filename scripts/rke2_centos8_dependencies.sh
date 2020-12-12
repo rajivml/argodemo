470dff58f5d7aca30266f17b360aabba30a97b00
rajivml <46923567+rajivml@users.noreply.github.com>
2020-12-12 16:05:11 +0530

    Update rke2_centos8_dependencies.sh

--- scripts/rke2_centos8_dependencies.sh
+++ scripts/rke2_centos8_dependencies.sh
@@ -45,57 +45,6 @@ CHECKSUM_ACTUAL=$(sha256sum "rke2-images.linux-amd64.tar.gz" | awk '{print $1}')
 if [ "${CHECKSUM_EXPECTED}" != "${CHECKSUM_ACTUAL}" ]; then echo "FATAL: download sha256 does not match"; exit 1; fi
 rm -f sha256sum-amd64.txt
 
-#dependencies to parse the yaml 
-pip3 install rq
-pip3 install jq
-
-
-#----------------- other deps start ####################
-mkdir other_deps
-cd other_deps
-
-#Fetch argocd yaml
-curl -LO https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
-#Save argocd images 
-images=(`cat install.yaml | yq -r '..|.image? | select(.)' | sort | uniq`)
-for image in ${images[@]};
-do
-  save_image=$(echo $image | rev | cut -d'/' -f1 | rev | tr ':' '_')
-  docker pull ${image}
-  #docker save ${image} | gzip > ${save_image}.tar.gz
-  docker save ${image} > ${save_image}.tar
-done
-
-#Package other_deps folder
-cd ..;
-tar -zcvf other_deps.tar.gz other_deps;
-rm -rf other_deps;
-
-#----------------- other deps end ####################
-
-
-#----------------- scripts start ####################
-mkdir scripts
-cd scripts
-curl -sL https://raw.githubusercontent.com/rancher/rke2/488bab0f48b848e408ce399c32e7f5f73ce96129/bundle/bin/rke2-uninstall.sh --output rke2-uninstall.sh
-chmod +x rke2-uninstall.sh
-
-curl -sL https://raw.githubusercontent.com/rancher/rke2/488bab0f48b848e408ce399c32e7f5f73ce96129/bundle/bin/rke2-killall.sh --output rke2-killall.sh
-chmod +x rke2-killall.sh
-
-#download k9s 
-curl -sL https://github.com/derailed/k9s/releases/download/v0.24.2/k9s_Linux_x86_64.tar.gz
-sleep 5
-tar xzvf k9s_Linux_x86_64.tar.gz
-rm k9s.tar.gz
-rm k9s_Linux_x86_64.tar.gz
-rm LICENSE
-rm README.md
-
-cd ..
-#----------------- scripts end ####################
-
 
 # install rke rpm repo
 cat <<-EOF >"/etc/yum.repos.d/rancher-rke2-latest.repo"
