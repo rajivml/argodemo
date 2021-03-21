18b97cfeac09261ac7b437f466ff86e299650f23
rajivml <46923567+rajivml@users.noreply.github.com>
2021-03-21 08:30:51 +0530

    secret copier

--- shell-operator/examples/104-secret-copier/build.sh
+++ shell-operator/examples/104-secret-copier/build.sh
@@ -1,3 +1,1 @@
-#**** DockerBuild on windows is screwing up permissions, but the build from linux is working fine, copy the zip file committed here to linux machine and build the same from there
-#find . -type f -print0 | xargs -0 dos2unix
 docker build -f ./Dockerfile -t uipath/secret-copier:0.1 .
